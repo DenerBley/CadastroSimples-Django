0e53ab292f781ae2f3d10f970694ec1a853fe2ba
DenerBley <deneriot@outlook.com>
2023-10-16 09:41:25 -0300

    DajngoProject

---
+++ app_cad_usuarios/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home(request):
+    return render(request,'usuarios/home.html')
+
+
+def usuarios(request):
+#Salvar dados da tela para interface 
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+#Exibir todos os usuarios ja cadastrados
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+#Retornar os dados para a pagina de listagem de usuarios 
+    return render (request, 'usuarios/usuarios.html', usuarios)
